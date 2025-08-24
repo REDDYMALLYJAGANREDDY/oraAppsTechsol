5ffb674b33d9d048752b4614f850a13ce615dceb
srinathgoud1 <srinathgoud272@gmail.com>
2025-08-24 17:27:48 +0530

    Commited

--- src/app/home/Navbar.tsx
+++ src/app/home/Navbar.tsx
@@ -4,6 +4,7 @@ import Symbollogo from "../../assets/images/oraappstech-symbol-logo.png";
 import Title from "../../assets/images/oraappstech-tit-logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -20,30 +21,68 @@ const Navbar = () => {
           +91 63033 22050
         </div>
       </div>
+
       <div className="Navthings">
         <div className="Nav">
-          <a href="#">
+          <NavLink to="/">
             <img className="symbollogo" src={Symbollogo} alt="symbol logo" />
             <img src={Title} alt="title-logo" className="titlelogo" />
-          </a>
+          </NavLink>
         </div>
+
         <ul className="Services">
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>SOFTWARE DEVELOPMENT</li>
+          <li>
+            <NavLink to="/" className="nav-link">
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about-us" className="nav-link">
+              ABOUT
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/software-development" className="nav-link">
+              SOFTWARE DEVELOPMENT
+            </NavLink>
+          </li>
 
           <li className="dropdown">
-            SERVICES ▼            
+            <span className="nav-link">SERVICES ▼</span>
             <ul className="dropdown-menu">
-              <a href="#">Consulting & Outsourcing</a>
-              <a href="#">UI/UX Design</a>
-              <a href="#">Trainings</a>
-              <a href="#">Project Support</a>
-              <a href="#">Gen AI Services</a>
+              <li>
+                <NavLink to="/ora-services/consulting-outsourcing" className="dropdown-link">
+                  Consulting & Outsourcing
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/ora-services/ux-ui-design" className="dropdown-link">
+                  UI/UX Design
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/ora-services/trainings" className="dropdown-link">
+                  Trainings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/ora-services/proj-support" className="dropdown-link">
+                  Project Support
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/ora-services/gen-ai" className="dropdown-link">
+                  Gen AI Services
+                </NavLink>
+              </li>
             </ul>
-            
           </li>
-          <li>CONTACT US</li>
+
+          <li>
+            <NavLink to="/contact-us" className="nav-link">
+              CONTACT US
+            </NavLink>
+          </li>
         </ul>
       </div>
     </div>
